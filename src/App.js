0fb89fdd06022e1fef78a2c2ca32ce4646ea6044
unknown <vncapit@gmail.com>
2023-04-06 23:01:28 +0800

    Todo react, init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleCheck,
+  faPen,
+  faTrashCan,
+} from "@fortawesome/free-solid-svg-icons";
 
 function App() {
+  const [toDo, setToDo] = useState([
+    { id: 1, title: "Task 1", status: false },
+    { id: 2, title: "Task 2", status: false },
+  ]);
+
+  const [newTask, setNewTask] = useState("");
+  const [updateData, setUpdateData] = useState("");
+
+  const addTask = () => {};
+
+  const deleteTask = (id) => {};
+
+  const markDone = (id) => {};
+
+  const cancelUpdate = () => {};
+
+  const changeTask = (e) => {};
+
+  const updateTask = () => {};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <br></br>
+      <h2>To Do List App</h2>
+      <br></br>
+
+      <div className="row">
+        <div className="col">
+          <input className="form-control form-control-lg" />
+        </div>
+        <div className="col-auto">
+          <button className="btn btn-lg btn-success me-4">Update</button>
+          <button className="btn btn-lg btn-warning">Cancel</button>
+        </div>
+      </div>
+      <br />
+
+      <div className="row">
+        <div className="col">
+          <input className="form-control form-control-lg" />
+        </div>
+        <div className="col-auto">
+          <button className="btn btn-lg btn-success">Add Task</button>
+        </div>
+      </div>
+
+      {toDo && toDo.length ? "" : "No Tasks..."}
+
+      {toDo &&
+        toDo
+          .sort((a, b) => (a.id > b.id ? 1 : -1))
+          .map((task, index) => {
+            return (
+              <React.Fragment key={task.id}>
+                <div className="col taskBg">
+                  <div className={task.status ? "done" : ""}>
+                    <span className="taskNumber">{index + 1}</span>
+                    <span className="taskText">{task.title}</span>
+                  </div>
+                  <div className="iconsWrap">
+                    <span title="Completed / Not Completed">
+                      <FontAwesomeIcon icon={faCircleCheck} />
+                    </span>
+                    <span title="Edit">
+                      <FontAwesomeIcon icon={faPen} />
+                    </span>
+                    <span title="Delete">
+                      <FontAwesomeIcon icon={faTrashCan} />
+                    </span>
+                  </div>
+                </div>
+              </React.Fragment>
+            );
+          })}
     </div>
   );
 }
