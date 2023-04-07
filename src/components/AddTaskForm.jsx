4761058445644fc5a0a5a960f890222c7bac18ce
unknown <vncapit@gmail.com>
2023-04-07 23:05:48 +0800

    done react todo app v1

--- src/components/AddTaskForm.jsx
+++ src/components/AddTaskForm.jsx
@@ -1,24 +1,24 @@
 const AddTaskForm = ({ newTask, setNewTask, addTask }) => {
-    return (
-        <div className="row">
-            <div className="col">
-                <input
-                    value={newTask}
-                    onChange={(e) => setNewTask(e.target.value)}
-                    className="form-control form-control-lg"
-                />
-            </div>
-            <div className="col-auto">
-                <button
-                    disabled={!newTask.trim()}
-                    onClick={() => addTask()}
-                    className="btn btn-lg btn-success"
-                >
-                    Add Task
-                </button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="row">
+      <div className="col">
+        <input
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          className="form-control form-control-lg"
+        />
+      </div>
+      <div className="col-auto">
+        <button
+          disabled={!newTask.trim()}
+          onClick={() => addTask()}
+          className="btn btn-lg btn-success"
+        >
+          Add Task
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default AddTaskForm;
--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,48 +1,49 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-    faCircleCheck,
-    faPen,
-    faTrashCan,
+  faCircleCheck,
+  faPen,
+  faTrashCan,
 } from "@fortawesome/free-solid-svg-icons";
 
 const ToDo = ({ toDo, markDone, editTask, deleteTask }) => {
-    toDo &&
+  return (
+    <div>
+      {toDo &&
         toDo
-            .sort((a, b) => (a.id > b.id ? 1 : -1))
-            .map((task, index) => {
-                return (
-                    <React.Fragment key={task.id}>
-                        <div className="col taskBg">
-                            <div className={task.status ? "done" : ""}>
-                                <span className="taskNumber">{index + 1}</span>
-                                <span className="taskText">{task.title}</span>
-                            </div>
-                            <div className="iconsWrap">
-                                <span
-                                    onClick={() => markDone(task.id)}
-                                    title="Completed / Not Completed"
-                                >
-                                    <FontAwesomeIcon icon={faCircleCheck} />
-                                </span>
-                                <span
-                                    hidden={task.status}
-                                    title="Edit"
-                                    onClick={() => editTask(task.id)}
-                                >
-                                    <FontAwesomeIcon icon={faPen} />
-                                </span>
-                                <span
-                                    title="Delete"
-                                    onClick={() => deleteTask(task.id)}
-                                >
-                                    <FontAwesomeIcon icon={faTrashCan} />
-                                </span>
-                            </div>
-                        </div>
-                    </React.Fragment>
-                );
-            });
+          .sort((a, b) => (a.id > b.id ? 1 : -1))
+          .map((task, index) => {
+            return (
+              <React.Fragment key={task.id}>
+                <div className="col taskBg">
+                  <div className={task.status ? "done" : ""}>
+                    <span className="taskNumber">{index + 1}</span>
+                    <span className="taskText">{task.title}</span>
+                  </div>
+                  <div className="iconsWrap">
+                    <span
+                      onClick={() => markDone(task.id)}
+                      title="Completed / Not Completed"
+                    >
+                      <FontAwesomeIcon icon={faCircleCheck} />
+                    </span>
+                    <span
+                      hidden={task.status}
+                      title="Edit"
+                      onClick={() => editTask(task.id)}
+                    >
+                      <FontAwesomeIcon icon={faPen} />
+                    </span>
+                    <span title="Delete" onClick={() => deleteTask(task.id)}>
+                      <FontAwesomeIcon icon={faTrashCan} />
+                    </span>
+                  </div>
+                </div>
+              </React.Fragment>
+            );
+          })}
+    </div>
+  );
 };
 
 export default ToDo;
