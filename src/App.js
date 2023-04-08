bbd86bcb7169378746aa1e0d8af3fac8b4fa4006
cap <vncapit@gmail.com>
2023-04-08 18:02:44 +0800

    Done todo react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useEffect, useState, memo } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import ToDo from "./components/ToDo";
@@ -7,100 +7,114 @@ import UpdateForm from "./components/UpdateForm";
 import AddTaskForm from "./components/AddTaskForm";
 
 function App() {
-  const [toDo, setToDo] = useState([]);
+    const [toDo, setToDo] = useState([]);
+    const [isFirstLoad, setIsFirstLoad] = useState(true);
 
-  const [newTask, setNewTask] = useState("");
-  const [updateData, setUpdateData] = useState({
-    isShow: false,
-    id: null,
-    title: "",
-  });
+    useEffect(() => {
+        if (isFirstLoad) {
+            let ls = localStorage.getItem("todos");
+            if (ls) {
+                setToDo(JSON.parse(ls));
+            }
+        }
+        if (!isFirstLoad) {
+            localStorage.setItem("todos", JSON.stringify(toDo));
+        }
+        setIsFirstLoad(false);
+    }, [toDo]);
 
-  const addTask = () => {
-    const newId = toDo.length ? toDo[toDo.length - 1].id + 1 : 0;
-    setToDo([...toDo, { id: newId, title: newTask, status: false }]);
-    setNewTask("");
-  };
-
-  const deleteTask = (id) => {
-    const newToDo = toDo.filter((item, index) => {
-      return item.id !== id;
+    const [newTask, setNewTask] = useState("");
+    const [updateData, setUpdateData] = useState({
+        isShow: false,
+        id: null,
+        title: "",
     });
-    setToDo([...newToDo]);
-  };
 
-  const markDone = (id) => {
-    let newTodo = toDo.map((item, index) => {
-      return item.id !== id ? item : { ...item, status: !item.status };
-    });
-    setToDo(newTodo);
-  };
+    const addTask = () => {
+        const newId = toDo.length ? toDo[toDo.length - 1].id + 1 : 0;
+        setToDo([...toDo, { id: newId, title: newTask, status: false }]);
+        setNewTask("");
+    };
 
-  const updateTask = () => {
-    if (updateData.id !== null) {
-      setToDo([
-        ...toDo.map((item, index) => {
-          return item.id !== updateData.id
-            ? item
-            : { ...item, title: updateData.title };
-        }),
-      ]);
-    }
-    setUpdateData({ isShow: false, id: null, title: "" });
-  };
+    const deleteTask = (id) => {
+        const newToDo = toDo.filter((item, index) => {
+            return item.id !== id;
+        });
+        setToDo([...newToDo]);
+    };
 
-  const editTask = (id) => {
-    setUpdateData({
-      ...updateData,
-      id: id,
-      isShow: true,
-      title: toDo.find((item, index) => item.id === id).title,
-    });
-  };
+    const markDone = (id) => {
+        let newTodo = toDo.map((item, index) => {
+            return item.id !== id ? item : { ...item, status: !item.status };
+        });
+        setToDo(newTodo);
+    };
 
-  const cancelUpdate = () => {
-    setUpdateData({ isShow: false, id: null, title: "" });
-  };
+    const updateTask = () => {
+        if (updateData.id !== null) {
+            setToDo([
+                ...toDo.map((item, index) => {
+                    return item.id !== updateData.id
+                        ? item
+                        : { ...item, title: updateData.title };
+                }),
+            ]);
+        }
+        setUpdateData({ isShow: false, id: null, title: "" });
+    };
 
-  return (
-    <div className="App container">
-      <br></br>
-      <h2>To Do List App</h2>
-      <br></br>
-      {/** Update task */}
-      {updateData.isShow ? (
-        <UpdateForm
-          updateData={updateData}
-          setUpdateData={setUpdateData}
-          updateTask={updateTask}
-          cancelUpdate={cancelUpdate}
-        />
-      ) : (
-        <AddTaskForm
-          newTask={newTask}
-          setNewTask={setNewTask}
-          addTask={addTask}
-        />
-      )}
+    const editTask = (id) => {
+        setUpdateData({
+            ...updateData,
+            id: id,
+            isShow: true,
+            title: toDo.find((item, index) => item.id === id).title,
+        });
+    };
 
-      {/** Display tasks */}
-      {toDo && toDo.length ? (
-        ""
-      ) : (
-        <div>
-          <br />
-          "No Tasks..."
+    const cancelUpdate = () => {
+        setUpdateData({ isShow: false, id: null, title: "" });
+    };
+
+    return (
+        <div className="App container">
+            <br></br>
+            <h2>To Do List App</h2>
+            <br></br>
+            {/** Update task */}
+            {updateData.isShow ? (
+                <UpdateForm
+                    updateData={updateData}
+                    setUpdateData={setUpdateData}
+                    updateTask={updateTask}
+                    cancelUpdate={cancelUpdate}
+                />
+            ) : (
+                <AddTaskForm
+                    newTask={newTask}
+                    setNewTask={setNewTask}
+                    addTask={addTask}
+                />
+            )}
+
+            {/** Display tasks */}
+            {toDo && toDo.length ? (
+                ""
+            ) : (
+                <div>
+                    <br />
+                    "No Tasks..."
+                </div>
+            )}
+
+            <ToDo
+                toDo={toDo}
+                markDone={markDone}
+                editTask={editTask}
+                deleteTask={deleteTask}
+            />
         </div>
-      )}
-
-      <ToDo
-        toDo={toDo}
-        markDone={markDone}
-        editTask={editTask}
-        deleteTask={deleteTask}
-      />
-    </div>
-  );
+    );
 }
 
 export default App;
